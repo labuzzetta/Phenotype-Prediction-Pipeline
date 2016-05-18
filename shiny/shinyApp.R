a4600ac7058f2d1d3566b5d63070559258a80329
clabuzze <clabuzze@iastate.edu>
2016-05-18 10:04:14 -0500

    Split ui into sections for readability

--- shiny/shinyApp.R
+++ shiny/shinyApp.R
@@ -3,25 +3,28 @@
 library(shiny)
 library(shinydashboard)
 
-ui <- dashboardPage(
-  dashboardHeader(title = "MVP Pipeline"),
-  dashboardSidebar(
-    menuItem("Predict Unknowns", tabName = "unknowns", icon = icon("magic"),
-             badgeLabel = "new", badgeColor = "green"),
-    menuItem("Cross Validation", tabName = "validation", icon = icon("line-chart"),
-             badgeLabel = "new", badgeColor = "green")
-  ),
-  dashboardBody(
-    tabItems(
-      tabItem(tabName = "unknowns",
-              h2("Predict Phenotype of Unknown Samples")
-      ),
-      
-      tabItem(tabName = "validation",
-              h2("Cross Validate Machine Learning Methods")
-      )
+sidebar <- dashboardSidebar(
+  menuItem("Predict Unknowns", tabName = "unknowns", icon = icon("magic"),
+           badgeLabel = "new", badgeColor = "green"),
+  menuItem("Cross Validation", tabName = "validation", icon = icon("line-chart"),
+           badgeLabel = "new", badgeColor = "green")
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "unknowns",
+            h2("Predict Phenotype of Unknown Samples")
+    ),
+    
+    tabItem(tabName = "validation",
+            h2("Cross Validate Machine Learning Methods")
     )
   )
+)
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = "MVP Pipeline"), sidebar, body
 )
 
 server <- function(input, output) {
