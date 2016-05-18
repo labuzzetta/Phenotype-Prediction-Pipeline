53ce585c70e0724b1e23862c22d9e0f5cc7752fb
clabuzze <clabuzze@iastate.edu>
2016-05-18 09:21:19 -0500

    Add outline of shiny app

---
+++ shiny/shinyApp.R
@@ -0,0 +1,15 @@
+library(shiny)
+ui <- fluidPage(pageWithSidebar(
+  headerPanel(
+    
+  ), 
+  sidebarPanel(
+    
+  ), 
+  mainPanel(
+    
+  )))
+server <- function(input, output) {
+  
+}
+shinyApp(ui, server)
