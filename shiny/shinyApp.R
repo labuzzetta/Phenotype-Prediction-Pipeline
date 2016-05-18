0ba6e0e08d5304a46fe4aeaa35bc2dfc5bf7a442
clabuzze <clabuzze@iastate.edu>
2016-05-18 10:01:10 -0500

    Add sidebar functions

--- shiny/shinyApp.R
+++ shiny/shinyApp.R
@@ -4,9 +4,24 @@ library(shiny)
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardHeader(title = "MVP Pipeline"),
+  dashboardSidebar(
+    menuItem("Predict Unknowns", tabName = "unknowns", icon = icon("magic"),
+             badgeLabel = "new", badgeColor = "green"),
+    menuItem("Cross Validation", tabName = "validation", icon = icon("line-chart"),
+             badgeLabel = "new", badgeColor = "green")
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "unknowns",
+              h2("Predict Phenotype of Unknown Samples")
+      ),
+      
+      tabItem(tabName = "validation",
+              h2("Cross Validate Machine Learning Methods")
+      )
+    )
+  )
 )
 
 server <- function(input, output) {
